6e7ca36166ed15292e290d4dbdb70675d1b18e2e
nina1603 <32387176+nina1603@users.noreply.github.com>
2018-09-24 14:25:52 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 conda env create -f env.yml -n myenv 
 source activate myenv 
-jupyter nbconvert —to html task_0.ipynb 
+jupyter —execute nbconvert —to html task_0.ipynb
 google-chrome task_0.html 
 source deactivate
